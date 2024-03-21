ff7083ee29d639565fe1fe08b05cd5973d997a41
eldor2003 <qulsoatoveldor@gmail.com>
2024-03-21 10:36:49 +0500

    desktop_full

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,16 +1,25 @@
-$(document).ready(function(){
-    
-    // hamburger
-    $(".hamurger_menu").on('click',function(e){
-        e.preventDefault();
-        $(".header_menus").addClass("active")
-    })
-    
-    $(".close").on('click',function(e){
-        e.preventDefault();
-        $(".header_menus").removeClass("active")
-    })
+$(document).ready(function () {
+	// hamburger
+	$(".hamurger_menu").on("click", function (e) {
+		e.preventDefault();
+		$(".header_menus").addClass("active");
+	});
 
+	$(".close").on("click", function (e) {
+		e.preventDefault();
+		$(".header_menus").removeClass("active");
+	});
 
-    // 
+	// swiper
+	var swiper = new Swiper(".mySwiper", {
+		slidesPerView: 1.5,
+		// centeredSlides: true,
+		centeredSlides: true,
+		spaceBetween: 20,
+		loop: true,
+		navigation: {
+			nextEl: ".swiper-button-next",
+			prevEl: ".swiper-button-prev",
+		},
+	});
 });
